b93c66b49399fa1815fa09f8e1e37c26720bfb0a
FENG Xikang <xikanfeng2@gmail.com>
2019-08-22 12:16:45 +0800

    delete scripts

--- scripts/run-magic.py
+++
@@ -1,8 +0,0 @@
-import magic
-import pandas as pd
-import sys
-
-data = pd.read_csv(sys.argv[1], index_col=0)
-magic_op = magic.MAGIC()
-emt_magic = magic_op.fit_transform(data.T, genes='all_genes')
-emt_magic.T.to_csv(sys.argv[2])
